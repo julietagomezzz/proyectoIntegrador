6eb65f3476f2b6716b0cad14c1ea9be677a276f8
paulamaggio <pmaggio@udesa.edu.ar>
2022-06-12 15:40:53 -0300

    conectando la API

--- js/home.js
+++ js/home.js
@@ -1,3 +1,14 @@
-let urlcanciones = "https://api.allorigins.win/raw?url=https://api.deezer.com/chart";
-
+let url = "https://api.allorigins.win/raw?url=https://api.deezer.com/chart";
 let canciones = " ";
+
+fetch(url)
+.then(function(response){
+  return response.json();
+})
+.then(function(data){
+  console.log(data);
+})
+.catch(function(error){
+  console.log(error);
+})
+
