cbcca9b2f51aeba08c5dbaf30426a26d801de0d6
paulamaggio <pmaggio@udesa.edu.ar>
2022-06-13 14:09:13 -0300

    api

--- js/home.js
+++ js/home.js
@@ -1,12 +1,27 @@
-let section = ('.sectionPadre') ;
-let url = 'https://api.allorigins.win/raw?url=https://api.deezer.com/chart';
+let urlcanciones = "https://api.allorigins.win/raw?url=https://api.deezer.com/chart";
 
-fetch(url)
-.then(function (response) {
-  return response.json();
-}) .then(function (data) {
-  console.log(data)
+let canciones = " ";
+
+fetch(urlcanciones)
+.then(function(response) {
+  return response.json()
 })
-.catch(function (error){
-console.log(error)
+.then(function(data) {
+  console.log(data);
+
+  let song = data.tracks.data;
+  canciones = document.querySelector(".cancionesAPI");
+
+  for (let i = 0; i < 5 ; i++) {
+
+    canciones.innerHTML += `<article>
+                            <img src="${song[i].artist.picture_medium}" alt='' />
+                            <p> ${song[i].title}</p>
+                            <p> ${song[i].album.title} </p>
+                            </article>`
+
+  }
+})
+.catch(function(error) {
+  console.log("Error: " + error);
 })
