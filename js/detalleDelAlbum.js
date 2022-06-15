e08959ef1a8ca19b5b8f88d54d82b5f354614d98
paulamaggio <pmaggio@udesa.edu.ar>
2022-06-14 23:30:51 -0300

    detalle del album

---
+++ js/detalleDelAlbum.js
@@ -0,0 +1,34 @@
+let queryString = location.search;
+
+let queryStringObjLiteral = new URLSearchParams(queryString);
+
+let id = queryStringObjLiteral.get('id');
+
+let urlAlbum = `https://api.allorigins.win/raw?url=https://api.deezer.com/album/${id}`
+
+let detalleDelAlbum = " ";
+
+fetch(urlAlbum)
+  .then(function (response) {
+    return response.json()
+  })
+  .then(function (data) {
+    console.log(data);
+
+    let album = data;
+    detalleDelAlbum = document.querySelector('.articleDetalle');
+    
+    detalleDelAlbum.innerHTML = `<article>
+                            <img class="imagenDetalle" src="${album.cover_big}" alt='' />
+                            <div class="divAlbum">
+                            <h4 class="articleAlbum articlesEnlaces">${album.title}</h4>
+                            <p class="articleAlbum articlesEnlaces"><a class="articlesEnlaces" href="./detalleDelArtista.html"></a> </p>
+                            <p class="articleAlbum articlesEnlaces">${album.genres}</p>
+                            <p class="articleAlbum articlesEnlaces">${album.release_date}</p>
+                            <p class="articleAlbum articlesEnlaces"> Canciones: </p>
+                            </div>
+                            </article>`
+  })
+  .catch(function (error) {
+    console.log("Error: " + error);
+  })
