5edf6feb8179a036027039eef59046701568be70
julietagomezzz <julietagomez@udesa.edu.ar>
2022-06-15 12:38:33 -0300

    generos: js y html

--- js/generos.js
+++ js/generos.js
@@ -1,23 +1,29 @@
-let urlcanciones = "https://api.allorigins.win/raw?url=https://api.deezer.com/chart";
+let queryString = location.search;
+
+let queryStringObjLiteral = new URLSearchParams(queryString);
+
+let id = queryStringObjLiteral.get('id');
+
+let urlGeneros = `https://api.allorigins.win/raw?url=https://api.deezer.com/genre`;
 
 let generos = " ";
 
-fetch(urlcanciones)
+fetch(urlGeneros)
   .then(function (response) {
     return response.json()
   })
   .then(function (data) {
     console.log(data);
 
-    let genero ;
+    let genero = data.data;
     generos = document.querySelector(".sectionGenero1");
 
 
-     for (let i = 0; i < 3; i++) {
+     for (let i = 1; i < 6; i++) {
     
       generos.innerHTML += `<article class="articleHome">
-                            <img class="imagenGenero" src="${song[i].artist.picture_medium}" alt='' />
-                            <h4 class="h4genero"><a href="./detalleDelGenero.html" class="articlesEnlaces">${artista[i].name}</a></h4>
+                            <img class="imagenes" src="${genero[i].picture_medium}" alt='fotoGeneros' />
+                            <h4 class="h4genero"><a href="./detalleDelGenero.html" class="articlesEnlaces">${genero[i].name}</a></h4>
                             </article>`
     }
 
