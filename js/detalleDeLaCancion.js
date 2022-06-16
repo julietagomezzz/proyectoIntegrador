604cf761dc7d9590a0b9a65c66d9abd639393618
paulamaggio <pmaggio@udesa.edu.ar>
2022-06-15 23:15:59 -0300

    detalle de la cancion y del album

--- js/detalleDeLaCancion.js
+++ js/detalleDeLaCancion.js
@@ -14,16 +14,16 @@ fetch(urlCancion)
   })
   .then(function (data) {
     console.log(data);
-
-    let cancion = data;
+  
+    let canciones = data;
     detalleDeLaCancion = document.querySelector('.sectionCancion');
-    
-      detalleDeLaCancion.innerHTML = `<article>
-                                    <img class="imagenDetalle" src="${cancion.album.cover_big}" alt='' />
+
+    detalleDeLaCancion.innerHTML = `<article>
+                                    <img class="imagenDetalle" src="${canciones.album.cover_big}" alt='' />
                                     <div class="divAlbum">
-                                    <h4 class="articleAlbum articlesEnlaces h3">${cancion.title}</h4>
-                                    <h5 class="articleAlbum articlesEnlaces h3">${cancion.album.title}</h5>
-                                    <p class="articleAlbum articlesEnlaces h3">${cancion.artist.name}</p>
+                                    <h4 class="articleAlbum articlesEnlaces h3">${canciones.title}</h4>
+                                    <h5 class="articleAlbum articlesEnlaces h3">${canciones.album.title}</h5>
+                                    <p class="articleAlbum articlesEnlaces h3">${canciones.artist.name}</p>
                                     <p class="articleAlbum articlesEnlaces"> Mi playlist </p>
                                     </div>
                                     </article>`
--- js/detalleDelAlbum.js
+++ js/detalleDelAlbum.js
@@ -6,13 +6,11 @@ let id = queryStringObjLiteral.get('id');
 
 let urlAlbum = `https://api.allorigins.win/raw?url=https://api.deezer.com/album/${id}`;
 
-let urlCancionesDelAlbum = `https://api.allorigins.win/raw?url=https://api.deezer.com/album/${id}/tracks`;
+let urlCancionesDelAlbum = `https://api.allorigins.win/raw?url=https://api.deezer.com/artist/${id}/top?limit=9`;
 
 let detalleDelAlbum = " ";
 let cancionesDelAlbum = " ";
 let cancionesDelAlbumHtml = document.querySelector("#a");
-
-console.log(id)
 
 fetch(urlAlbum)
   .then(function (response) {
@@ -21,19 +19,19 @@ fetch(urlAlbum)
   .then(function (data) {
     console.log(data);
 
-    let album = data.data;
+    let album = data;
     detalleDelAlbum = document.querySelector('.articleDetalleDelAlbum');
 
     detalleDelAlbum.innerHTML += `<article>
-                            <img class="imagenDetalle" src="${album.cover_big}" alt='' />
-                            <div class="divAlbum">
-                            <h4 class="articleAlbum articlesEnlaces">${album.title}</h4>
-                            <p class="articleAlbum articlesEnlaces"><a class="articlesEnlaces" href="./detalleDelArtista.html?id=${album[i].album.id}">${album.artist.name}</a></p>
-                            <p class="articleAlbum articlesEnlaces">${album.genres.data[i].name}</p>
-                            <p class="articleAlbum articlesEnlaces">${album.release_date}</p>
-                            <p class="articleAlbum articlesEnlaces"> Canciones: </p>
-                            </div>
-                            </article>`
+                                  <img class="imagenDetalle" src="${album.cover_big}" alt='' />
+                                  <div class="divAlbum">
+                                  <h4 class="articleAlbum articlesEnlaces">${album.title}</h4>
+                                  <p class="articleAlbum articlesEnlaces"><a class="articlesEnlaces" href="./detalleDelArtista.html?id=${album.artist.id}">${album.artist.name}</a></p>
+                                  <p class="articleAlbum articlesEnlaces">${album.genres.data.name}</p>
+                                  <p class="articleAlbum articlesEnlaces">${album.release_date}</p>
+                                  <p class="articleAlbum articlesEnlaces"> Canciones: </p>
+                                  </div>
+                                  </article>`
   })
   .catch(function (error) {
     console.log("Error: " + error);
@@ -44,17 +42,18 @@ fetch(urlAlbum)
     return response.json()
   })
   .then(function (data) {
-    console.log(data);
+    console.log(data.data);
 
-    let cancionesAlbum = data;
-    console.log(cancionesAlbum.length);
+    let cancionesAlbum = data.data;
+
+    console.log(cancionesAlbum.length)
 
     for (let i = 0; i < cancionesAlbum.length; i++) {
       console.log(i)
-      cancionesDelAlbum += `<li><a class="articlesEnlaces" href="./detalleDeLaCancion.html?id=${cancionesAlbum[i].id}"></a></li>`
-    }
-    
-    detalleDelAlbum.innerHTML += ``
+      cancionesDelAlbum += `<li><a class="articlesEnlaces" href="./detalleDeLaCancion.html?id=${cancionesAlbum[i].id}">${cancionesAlbum[i].title}</a></li>`
+       
+     }
+     cancionesDelAlbumHtml.innerHTML = cancionesDelAlbum;
   })
   .catch(function (error) {
     console.log("Error: " + error);
