1e0c05bc52ded512f290c4fb83135b8ac88eb357
paulamaggio <pmaggio@udesa.edu.ar>
2022-06-16 23:38:42 -0300

    js del detalle de generos y detalle del album

---
+++ js/detalleDelGenero.js
@@ -0,0 +1,38 @@
+let queryString = location.search;
+
+let queryStringObjLiteral = new URLSearchParams(queryString);
+
+let id = queryStringObjLiteral.get('id');
+
+let urlGeneros = `https://api.allorigins.win/raw?url=https://api.deezer.com/genre/${id}/artists`;
+
+let detalleDelGenero = " ";
+
+let generosHtml = document.querySelector(".sectionGenero");
+
+fetch(urlGeneros)
+  .then(function (response) {
+    return response.json()
+  })
+  .then(function (data) {
+    console.log(data.data);
+
+    let detalleDelGenero = data.data;
+
+    console.log(detalleDelGenero.length)
+
+    for (let i = 0; i < detalleDelGenero.length; i++) {
+      console.log(i)
+      detalleDelGenero +=  `<article>
+                            <img class="imagenes" src="${detalleDelGenero.cover_big}" alt="Adele">
+                            <h4 class="articlesParrafos fondoGenero"><a class="articlesEnlaces" href="./detalleDelArtista.html?id=${detalleDelGenero[i].id}">${detalleDelGenero[i].title}</a></h4>
+                            </article>`       
+     }
+
+     generosHtml.innerHTML = detalleDelGenero;
+
+  })
+  
+  .catch(function (error) {
+    console.log("Error: " + error);
+  })
